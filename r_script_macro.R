b5a825d68e6f508ab248415da7308af026f36f5b
DavidChaparro11 <d.chaparro@uniandes.edu.co>
2023-02-26 21:28:20 -0500

    a

--- r_script_macro.R
+++ r_script_macro.R
@@ -1,4 +1,4 @@
-
+rm(list=ls())
 require(pacman)
 p_load(haven, ggplot2, dplyr, tidyverse)
 base <- read_dta("PWT90.dta")
@@ -15,8 +15,24 @@ grafica <- ggplot(base_colombia, aes(year,ln_gdp)) +
   scale_x_continuous(breaks=seq(1950,2014, by=10))
 grafica
 ##grafica 2
+##africa
+africa_subsahariana <- c("Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Camerún", "República Centroafricana", "Chad", "Comoras", "Congo", "Costa de Marfil", "Yibuti", "República Democrática del Congo", "Guinea Ecuatorial", "Eritrea", "Etiopía", "Gabón", "Gambia", "Ghana", "Guinea", "Guinea-Bissau", "Kenia", "Lesoto", "Liberia", "Madagascar", "Malawi", "Malí", "Mauritania", "Mauricio", "Mozambique", "Namibia", "Níger", "Nigeria", "Ruanda", "Santo Tomé y Príncipe", "Senegal", "Seychelles", "Sierra Leona", "Somalia", "Sudáfrica", "Sudán del Sur", "Sudán", "Tanzania", "Togo", "Uganda", "Zambia", "Zimbabue")
+africa_subsahariana_pib <- base %>% filter(country %in% africa_subsahariana)
+africa_subsahariana_promedio <- africa_subsahariana_pib %>% group_by(year) %>% summarize(gdp_per_capita_us_100 = mean(gdp_per_capita_us_100))
+africa_subsahariana_promedio <- africa_subsahariana_promedio %>% mutate(country="Sub-Saharan Africa")
+africa_subsahariana_promedio <-  africa_subsahariana_promedio %>% select(country, year, gdp_per_capita_us_100)
+base_general <- base %>% select(country, year, gdp_per_capita_us_100)
+base_Africa <- rbind(base_general,africa_subsahariana_promedio)
+##Western Europe
+west_europe <- c("Austria", "Belgium", "France", "Germany", "Ireland", "Italy", "Luxembourg", "Netherlands", "Portugal", "Spain", "Switzerland", "United Kingdom")
+west_europe <- base %>% filter(country %in% west_europe)
+west_europe_p <- west_europe %>% group_by(year) %>% summarize(gdp_per_capita_us_100 = mean(gdp_per_capita_us_100))
+west_europe_p <- west_europe_p %>% mutate(country = "Western Europe") %>% select(country, year, gdp_per_capita_us_100)
+base_general <- rbind(base_Africa, west_europe_p)
 paises <- c("Brazil", "United States", "Colombia", "India", "China","Russian Federation","Western Europe", "Japan", "Sub-Saharan Africa")
-base_general <- base %>% filter(year >= 1980, country %in% paises)
+base_general <- base_general %>% filter(year >= 1980, country %in% paises)
+paises <- c("Brazil", "United States", "Colombia", "India", "China","Russian Federation","Western Europe", "Japan", "Sub-Saharan Africa")
+base_general <- base_general %>% filter(year >= 1980, country %in% paises)
 graf_2 <- ggplot(base_general, aes(x=year, y=gdp_per_capita_us_100, color=country))+
   geom_line(size=1)+
   geom_path(size=0.4, alpha=0.8)+
